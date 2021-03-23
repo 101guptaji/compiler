99015e350420269eb0d7a5dd5ae5d815f4363f33
Himanshu Gupta <43908946+101guptaji@users.noreply.github.com>
2021-03-23 23:00:47 +0530

    Trial codes

---
+++ output.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main() 
+{ 
+	int i; 
+	
+	while(i<=10)
+	{
+		printf( "Hello World")
+if(i==2){printf("%d",i);i++;
+	}	
+		
+	i++;
+	} 
+
+	return 0; 
+} 
+
---
+++ test1.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main() 
+{ 
+	int i=0; 
+	
+	for (i = 1; i <= 10; i++) 
+	{ 
+		for(int j=0;j<15;j++){
+		printf("%d",j);
+		}
+		printf( "Hello World\n");	 
+	} 
+
+	return 0; 
+} 
+
